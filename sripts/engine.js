0f882449bdf286f8b74c12910b8a70067ec587f6
Moroz Vladislav <nikeblack97@gmail.com>
2019-05-11 22:42:03 +0300

    implemented: canvas drawing (field, snake, apple), snake moves, snake growth

--- sripts/engine.js
+++ sripts/engine.js
@@ -2,36 +2,53 @@
 /**
  * Created by vlad on 11.05.2019.
  */
-
+const directions ={
+    EAST: { x:1, y:0 },
+    WEST: { x:-1, y:0},
+    NORTH: { x:0, y:-1},
+    SOUTH: { x:0, y:1}
+};
 const getApple = ()=> ({
-                          x: Math.floor(Math.random() * 100),
-                          y: Math.floor(Math.random() * 100)
+                          x: Math.ceil(Math.random() * 20)*5,
+                          y: Math.ceil(Math.random() * 20)*5
                        });
-const state = {
+const firstState = () => ({
     direction: "EAST",
-    position: {
+    head: {
       x: 50,
       y: 50
     },
     apple: getApple(),
     tail: []
-};
+});
 
-const directions ={
-    EAST: { x:1, y:0 },
-    WEST: { x:-1, y:0},
-    NORTH: { x:0, y:1},
-    SOUTH: { x:0, y:-1}
-};
-const next = ( prevState = state )=> {
-    const prevStateCopy = JSON.parse( JSON.stringify(prevState) );
-    const newState = {
-            ...prevStateCopy,
-            position:{
-                x: prevStateCopy.position.x + directions[prevStateCopy.direction].x,
-                y: prevStateCopy.position.y + directions[prevStateCopy.direction].y
-            }
+
+
+const computeNextState = (state )=> {
+    const prevState = JSON.parse( JSON.stringify(state) );
+    const foundApple = () => head.x === prevState.apple.x && head.y === prevState.apple.y;
+    const reviewTail = () => {
+        if( foundApple() ){
+            return [ {...prevState.head}, ...prevState.tail ]
+        }else{
+            return prevState.tail.length
+                             ? [ {...prevState.head}, ...prevState.tail.slice(0, -1) ]
+                             : prevState.tail;
+        }
     };
-    return newState;
+    const head = {
+        x: prevState.head.x + directions[prevState.direction].x*5,
+        y: prevState.head.y + directions[prevState.direction].y*5
+    };
+    const tail = reviewTail();
+    const apple = foundApple() ? getApple() : {...prevState.apple};
+    const newState = {
+                ...prevState,
+                head: head,
+                tail: tail,
+                apple: apple
+           };
+    return newState
 };
+const setNextDirection = ( state, newDirection)=> ({ ...state, direction: newDirection});
 
--- sripts/presenter.js
+++ sripts/presenter.js
@@ -2,4 +2,55 @@
 /**
  * Created by vlad on 11.05.2019.
  */
-let stateDraw = state;
+let STATE = firstState();
+const NEXT = computeNextState;
+const nextDIRECTION = setNextDirection;
+
+
+const canvas = document.getElementById('snake');
+const ctx = canvas.getContext('2d');
+
+const sizeCoefficients = {
+                            width: canvas.width/100,
+                            height: canvas.height/100
+                         };
+const computeSizes = ( { x, y } ) => ( { x: x*sizeCoefficients.width, y: y*sizeCoefficients.height } );
+
+const draw = ( ) => {
+    const x1 = X => computeSizes( X ).x;
+    const x2 = -sizeCoefficients.width*5;
+    const y1 = Y => computeSizes( Y ).y;
+    const y2 = -sizeCoefficients.height*5;
+
+    ctx.fillStyle = '#232323';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // draw snake
+     ctx.fillStyle = 'rgb(0,200,50)';
+     [ {...STATE.head}, ...STATE.tail ].forEach( cube => ctx.fillRect(x1(cube), y1(cube), x2, y2) );
+
+    // draw apples
+    ctx.fillStyle = 'rgb(255,50,0)';
+    ctx.fillRect(x1(STATE.apple), y1(STATE.apple), x2, y2)
+
+    // add crash
+    // if (state.snake.length == 0) {
+    //     ctx.fillStyle = 'rgb(255,0,0)'
+    //     ctx.fillRect(0, 0, canvas.width, canvas.height)
+    // }
+};
+const step = () => {
+    STATE = NEXT( STATE );
+    draw();
+};
+draw();
+
+
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+        case 'w': case 'h': case 'ArrowUp':    STATE = nextDIRECTION(STATE, "NORTH"); break;
+        case 'a': case 'j': case 'ArrowLeft':  STATE = nextDIRECTION(STATE, "WEST");  break;
+        case 's': case 'k': case 'ArrowDown':  STATE = nextDIRECTION(STATE, "SOUTH"); break;
+        case 'd': case 'l': case 'ArrowRight': STATE = nextDIRECTION(STATE, "EAST");  break
+    }
+});
