1c18b78e20784b956becf43ff015fb211cf26316
Moroz Vladislav <nikeblack97@gmail.com>
2019-05-12 23:47:39 +0300

    implemented game over scenario with animation

--- sripts/engine.js
+++ sripts/engine.js
@@ -8,10 +8,7 @@ const directions ={
     NORTH: { x:0, y:-1},
     SOUTH: { x:0, y:1}
 };
-const getApple = ()=> ({
-                          x: Math.ceil(Math.random() * 20)*5,
-                          y: Math.ceil(Math.random() * 20)*5
-                       });
+const getApple = ()=> ({ x: Math.ceil(Math.random() * 20)*5, y: Math.ceil(Math.random() * 20)*5 });
 const firstState = () => ({
     direction: "EAST",
     head: {
@@ -19,38 +16,43 @@ const firstState = () => ({
       y: 50
     },
     apple: getApple(),
-    tail: []
+    tail: [],
+    crashed: false
 });
-
-
-
-const computeNextState = (state )=> {
+const foundApple = ( head, apple ) => head.x === apple.x && head.y === apple.y;
+const computeNextState = ( state )=> {
     const prevState = JSON.parse( JSON.stringify(state) );
-    const foundApple = () => head.x === prevState.apple.x && head.y === prevState.apple.y;
-    // TODO: - compute crash
-    const reviewTail = () => {
-        if( foundApple() ){
-            return [ {...prevState.head}, ...prevState.tail ]
-        }else{
-            return prevState.tail.length
-                             ? [ {...prevState.head}, ...prevState.tail.slice(0, -1) ]
-                             : prevState.tail;
-        }
-    };
+    const appleX = prevState.apple.x;
+    const appleY = prevState.apple.y;
     const head = {
         x: prevState.head.x + directions[prevState.direction].x*5,
         y: prevState.head.y + directions[prevState.direction].y*5
     };
-    // TODO: - compute lefting borders
-    const tail = reviewTail();
-    const apple = foundApple() ? getApple() : {...prevState.apple};
+    const reviewTail = ( state ) => {
+        if( foundApple( head, { x: appleX, y: appleY }) ){
+            return [ {...state.head}, ...state.tail ]
+        }else if( state.tail.length){
+            return  [ {...state.head}, ...state.tail.slice(0, -1) ]
+        }else{
+            return state.tail
+        }
+    };
+    const leftedX = (head.x > 100 ? head.x = 0 : false) || (head.x < 0 ? head.x = 100 : false) ;
+    head.x = leftedX ? leftedX : head.x;
+    const leftedY = (head.y > 100 ? head.y = 0 : false) || (head.y < 0 ? head.y = 100 : false) ;
+    head.y = leftedY ? leftedY : head.y;
+    const tail = reviewTail( prevState );
+    const computeCrash = ( tail ) => tail.filter( cube => head.x === cube.x && head.y === cube.y ).length;
+    const apple = foundApple( head, { x: appleX, y: appleY }) ? getApple() : {...prevState.apple};
+    const crashed = !!computeCrash( tail );
     const newState = {
                 ...prevState,
                 head: head,
                 tail: tail,
-                apple: apple
+                apple: apple,
+                crashed: crashed
            };
-    return newState
+    return newState;
 };
 
 
--- sripts/presenter.js
+++ sripts/presenter.js
@@ -1,61 +1,69 @@
 "use strict";
-/**
- * Created by vlad on 11.05.2019.
- */
-const STATE = firstState();
-const NEXT = computeNextState;
-
-
+const STATE = firstState();     // "imported" function from another file
+const NEXT = computeNextState;  // "imported" function from another file
 const canvas = document.getElementById('snake');
 const ctx = canvas.getContext('2d');
-
-const sizeCoefficients = {
-                            width: canvas.width/100,
-                            height: canvas.height/100
-                         };
+const sizeCoefficients = { width: (canvas.width)/100, height: (canvas.height)/100 };
 const computeSizes = ( { x, y } ) => ( { x: x*sizeCoefficients.width, y: y*sizeCoefficients.height } );
-
+const delay = ms => new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve()
+    }, ms)
+});
 const draw = ( newState ) => {
     const x1 = X => computeSizes( X ).x;
     const x2 = -sizeCoefficients.width*5;
     const y1 = Y => computeSizes( Y ).y;
     const y2 = -sizeCoefficients.height*5;
-
     ctx.fillStyle = '#232323';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    // draw snake
+    // draw tail
      ctx.fillStyle = 'rgb(0,200,50)';
-     [ {...newState.head}, ...newState.tail ].forEach( cube => ctx.fillRect(x1(cube), y1(cube), x2, y2) );
-
-    // draw apples
+     [  ...newState.tail ].forEach( cube => ctx.fillRect(x1(cube), y1(cube), x2, y2) );
+    // draw apple
     ctx.fillStyle = 'rgb(255,50,0)';
-    ctx.fillRect(x1(newState.apple), y1(newState.apple), x2, y2)
-
-    /* TODO: - add crash
-     if (state.snake.length == 0) {
-         ctx.fillStyle = 'rgb(255,0,0)'
-         ctx.fillRect(0, 0, canvas.width, canvas.height)
-     } */
+    ctx.fillRect(x1(newState.apple), y1(newState.apple), x2, y2);
+    // draw head
+    ctx.fillStyle = 'rgb(255,140,0)';
+    [ {...newState.head}].forEach( cube => ctx.fillRect(x1(cube), y1(cube), x2, y2) );
+    if( newState.crashed ) {
+        ctx.fillStyle = 'rgb(255,0,0)';
+        return Promise.all([  ...newState.tail ]
+                        .map( (cube, i) => delay(150*(i+1)).then( ()=> ctx.fillRect(x1(cube), y1(cube), x2, y2) ) )
+            )
+            .then( () => delay(100) )
+            .then( () => {
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        });
+    }
+    return Promise.resolve()
 };
 const step = () => {
-    const newState = NEXT( STATE );
-    STATE.head = newState.head;
-    STATE.tail = newState.tail;
-    STATE.apple = newState.apple;
-    draw( STATE );
-    console.log("GOTHC YA! ");
-    // step( newState );
-    // TODO: - add animation fragment
+        const newState = NEXT(STATE);
+        if(newState.crashed){
+            draw( newState ).then( () => delay(3000)).then( () => {
+                const newState = firstState();
+                STATE.direction = newState.direction;
+                STATE.tail = newState.tail;
+                STATE.head = newState.head;
+                STATE.apple = newState.apple;
+                STATE.crashed = newState.crashed;
+                return draw(newState)
+            }).then(() => delay(100)).then(() => step())
+        }else {
+            STATE.head = newState.head;
+            STATE.tail = newState.tail;
+            STATE.apple = newState.apple;
+            draw( newState ).then(() => delay(100)).then(() => step())
+        }
 };
-draw( STATE );
-
-
+draw(STATE);
+delay(100).then( () => step() );
 window.addEventListener('keydown', e => {
     switch (e.key) {
-        case 'w': case 'h': case 'ArrowUp':    STATE.direction =  "NORTH"; break;
-        case 'a': case 'j': case 'ArrowLeft':  STATE.direction = "WEST"; break;
-        case 's': case 'k': case 'ArrowDown':  STATE.direction ="SOUTH"; break;
-        case 'd': case 'l': case 'ArrowRight': STATE.direction = "EAST";  break
+        case 'w': case'ц': case 'h':case'р': case 'ArrowUp':    STATE.direction = "NORTH"; break;
+        case 'a': case'ф': case 'j':case'о': case 'ArrowLeft':  STATE.direction = "WEST"; break;
+        case 's': case'ы': case 'k':case'л': case 'ArrowDown':  STATE.direction = "SOUTH"; break;
+        case 'd': case'в': case 'l':case'д': case 'ArrowRight': STATE.direction = "EAST";  break
     }
 });
